635082fdcb696b976bb047a7fc4bf01ce5f3355a
Tigran <tigranisajanyan@gmail.com>
2015-02-25 14:53:19 +0400

    my first commit

--- app/src/main/java/pArtapibeta/Photo.java
+++ app/src/main/java/pArtapibeta/Photo.java
@@ -7,6 +7,7 @@ import java.util.Date;
  * Created by Arman on 2/23/15.
  */
 public class Photo {
+
     private String id;
     private URL url;
     private String title;
@@ -26,53 +27,6 @@ public class Photo {
     private Location location;
 
 
-    public String getTitle() {
-        return title;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public void updateData(){
-        //TODO
-    }
-
-    public void comment(String comment){
-        //TODO
-    }
-
-    public Comment[] getComments(){
-
-    //TODO
-        return new Comment[1];
-    }
-
-    public Comment getCommentByid(String id){
-        //TODO
-        return new Comment(null,null,null);
-    }
-
-    public void removeComment(String id){
-
-       //TODO
-    }
-
-    /*public User[] getLikes(){
-        //TODO
-
-    }*/
-
-    public boolean like(){
-        //TODO
-        return false;
-    }
-
-    public boolean unLike(){
-        //TODO
-        return false;
-    }
-
 
     public Photo(String id, URL url, String title, Tag[] tags, Date crrated, boolean isMature, int width, int height, int likesCount, int viewsCount, int commentsCount, int repostsCount, boolean isLiked, boolean isReposted, String ownerid, Location location) {
         this.id = id;
@@ -81,6 +35,7 @@ public class Photo {
         this.tags = tags;
         this.created = crrated;
         this.isMature = isMature;
+
         this.width = width;
         this.height = height;
         this.likesCount = likesCount;
@@ -117,5 +72,139 @@ public class Photo {
 
     }
 
+    public String getTitle() {
+        return title;
+    }
 
+    public String getId() {
+        return id;
+    }
+
+    public URL getUrl() {
+        return url;
+    }
+
+    public Tag[] getTags() {
+        return tags;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public boolean isMature() {
+        return isMature;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setUrl(URL url) {
+        this.url = url;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setTags(Tag[] tags) {
+        this.tags = tags;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public void setMature(boolean isMature) {
+        this.isMature = isMature;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setLikesCount(int likesCount) {
+        this.likesCount = likesCount;
+    }
+
+    public void setViewsCount(int viewsCount) {
+        this.viewsCount = viewsCount;
+    }
+
+    public void setCommentsCount(int commentsCount) {
+        this.commentsCount = commentsCount;
+    }
+
+    public void setRepostsCount(int repostsCount) {
+        this.repostsCount = repostsCount;
+    }
+
+    public void setLiked(boolean isLiked) {
+        this.isLiked = isLiked;
+    }
+
+    public void setReposted(boolean isReposted) {
+        this.isReposted = isReposted;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public void setOwnerID(String ownerID) {
+        this.ownerID = ownerID;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getLikesCount() {
+        return likesCount;
+    }
+
+    public int getViewsCount() {
+        return viewsCount;
+    }
+
+    public int getCommentsCount() {
+        return commentsCount;
+    }
+
+    public int getRepostsCount() {
+        return repostsCount;
+    }
+
+    public boolean isLiked() {
+        return isLiked;
+    }
+
+    public boolean isReposted() {
+        return isReposted;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public String getOwnerID() {
+        return ownerID;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
 }
