91fb20b48e074c07fa6bf1544aaaa336ec0d08d4
Tigran <tigranisajanyan@gmail.com>
2015-03-20 19:08:21 +0400

    user test changing 2

--- app/src/main/java/pArtapibeta/PicsArtConst.java
+++ app/src/main/java/pArtapibeta/PicsArtConst.java
@@ -2,16 +2,15 @@ package pArtapibeta;
 
 /**
  * Created by Arman on 2/9/15.
- *
  */
 public class PicsArtConst {
 
-    public static final String CLIENT_ID = "WorkingClientPYzCIqGMBt0xx4fV";
-    public static final String CLIENT_SECRET = "Efm7ASWYgAZQ81HkbVhl6EdogwTn8d5c";
+    public static final String CLIENT_ID = "testtesttest1180eODOG3cZUUK2BN";
+    public static final String CLIENT_SECRET = "o1LafsilY9ZmUq9Z8IKmJi4fgTTxwmSb";
     public static final String REDIRECT_URI = "localhost";
     public static final String GRANT_TYPE = "authorization_code";
-    public static final String TOKEN_URL = "http://stage.i.picsart.com/api/oauth2/token";
-    public static final String OAUTH_URL = "http://stage.i.picsart.com/api/oauth2/authorize";
+    public static final String TOKEN_URL = "http://picsart.com/api/oauth2/token";
+    public static final String OAUTH_URL = "https://picsart.com/api/oauth2/authorize";
     public static final String TOKEN_URL_PREFIX = ".json?token=";
     public static final String MY_PROFILE_URL = "http://stage.i.picsart.com/api/users/me";
     public static final String USE_PROFILE_URL = "http://stage.i.picsart.com/api/users/";
@@ -19,19 +18,21 @@ public class PicsArtConst {
     public static final String Get_PHOTO_URL_PUB = "https://api.picsart.com/photos/show/";
     public static final String TOKEN_PREFIX = "?token=";
 
-
     public static final String OFFSET = "&offset=";
     public static final String LIMIT = "&limit=";
+
     public static final String Photo_UPLOAD_URL = "https://stage.i.picsart.com/api/photos/";
     public static final String GET_PHOTO_FILTER = "?filter=";
-   // public static final String APIKEY = "cd371244-3887-405b-82d1-7aadcb2617b9";
-    public static final String APIKEY = "d97fb4fa-63e5-4a59-a84a-5124376b11ed";
+    public static final String APIKEY = "cd371244-3887-405b-82d1-7aadcb2617b9";
+    // public static final String APIKEY = "d97fb4fa-63e5-4a59-a84a-5124376b11ed";
     public static final String API_PREFX = "?key=";
     public static final String PHOTO_UPLOAD_URL = "https://api.picsart.com/photos/add.json";
     public static final String PHOTO_COMMENT_URL = "https://api.picsart.com/photos/show/";
     public static final String PHOTO_COVER_URL = "https://api.picsart.com/users/cover/add.json";
     public static final String PHOTO_AVATAR_URL = "https://api.picsart.com/users/photo/add.json";
 
+    public static final String PHOTO_GENERAL_PREFIX = "http://picsart.com/api/photos/";
+    public static final String PHOTO_PRE_URL = "https://picsart.com/api/photos/";
     public static final String PHOTO_COMMENTS_URL = "https://api.picsart.com/photos/comments/show/";
     public static final String PHOTO_ADD_COMMENT_URL = "https://api.picsart.com/photos/comments/add/";
     public static final String PHOTO_REMOVE_COMMENT_URL = "https://api.picsart.com/photos/comments/remove/";
@@ -45,23 +46,31 @@ public class PicsArtConst {
     public static final String API_TEST_PREF = ".json?key=";
 
     public static final String GET_USER_PHOTOS_LIST = "http://stage.i.picsart.com/api/users/";
-    public static final String BLOCK_USER_WITH_ID = "http://stage.i.picsart.com/api/users/161436357000102/";//161436357000102/blocks?token=";
+
+    public static final String BLOCK_USER_WITH_ID = "https://api.picsart.com/blocks/add/";//161436357000102/blocks?token=";
+    //POST /users/{id}/blocks
     public static final String UNBLOCK_USER_WITH_ID = "http://stage.i.picsart.com/api/users/161436357000102/blocks/161263489000102?token=";
     public static final String FOLLOW_USER_WITH_ID = "http://stage.i.picsart.com/api/users/161436357000102/following?token=";
 
 
-    public static final String SHOW_USER_URL = "https://api.picsart.com/users/show/";
-    public static final String SHOW_USER_FOLLOWERS = "https://api.picsart.com/followers/show/";
-    public static final String SHOW_USER_FOLLOWING = "https://api.picsart.com/following/show/";
-    public static final String SHOW_USER_LIKED_PHOTOS = "https://api.picsart.com/users/likes/show/";
-    public static final String SHOW_USER_TAGS = "https://api.picsart.com/users/photos/tags/";
-    public static final String SHOW_USER_PLACES = "https://api.picsart.com/users/photos/places/";
-    public static final String SHOW_BLOCKED_USERS = "https://api.picsart.com/blocks/show/";
-    public static final String SHOW_USER_PHOTOS_LIST = "https://api.picsart.com/users/photos/show/";
+    public static final String SHOW_USER = "https://picsart.com/api/users/";
+    //public static final String SHOW_USER_FOLLOWERS = "https://picsart.com/api/users/";
+    public static final String FOLLOWERS_PREFIX = "/followers";
+    //public static final String SHOW_USER_FOLLOWING = "https://picsart.com/api/users/";
+    public static final String FOLLOWING_PREFIX = "/following";
+    //public static final String SHOW_USER_LIKED_PHOTOS = "https://picsart.com/api/users/";  //users/{id}/likes
+    public static final String LIKED_PHOTOS_PREFIX = "/likes";
+    //public static final String SHOW_USER_TAGS = "https://api.picsart.com/users/photos/tags/";
+    public static final String TAGS_PREFIX = "/tags";
+    //public static final String SHOW_USER_PLACES = "https://api.picsart.com/users/photos/places/";
+    public static final String PLACES_PREFIX = "/places";
+    //public static final String SHOW_BLOCKED_USERS = "https://api.picsart.com/blocks/show/";
+    public static final String BLOCKED_PREFIX = "/blocks";
+    //public static final String SHOW_USER_PHOTOS_LIST = "https://api.picsart.com/users/photos/show/";
+    public static final String PHOTOS_PREFIX = "/photos";
 
-    public static final String DATE_FORMAT="yyyy-MM-dd'T'hh:mm:ss.SSS'X'";
 
-    public static final  String[] paramsUserProfile =
+    public static final String[] paramsUserProfile =
             new String[]{
                     "username",         //0
                     "name",             //1
--- app/src/main/java/pArtapibeta/UserFactory.java
+++ app/src/main/java/pArtapibeta/UserFactory.java
@@ -48,44 +48,48 @@ public class UserFactory {
     }
 
     /**
-     * @param object Object
+     * @param o Object
      * @param offset starting point
      * @param limit  limit of users
      * @return ArrayList<User>
      *
      * Tries to Parse Object to ArrayList of User instances
      */
-    public static ArrayList<User> parseFromAsArray(Object object, int offset, int limit, String keyword) {
+    public static ArrayList<User> parseFromAsArray(Object o, int offset, int limit,String keyword) {
 
         ArrayList<User> userArrayList = new ArrayList<>();
         User nwUs = null;
         Location loc;
         Gson gson;
-        int max_limit;
-        JSONArray jarr=null;
-        JSONObject jsonObj=(JSONObject)object;
 
         try {
-
-            if(keyword!=null || keyword!=""){
+            JSONObject jsonObj = (JSONObject)o;
+            gson = new Gson();
+            JSONArray jarr=null;
+            if(keyword!=null|| keyword!=""){
                 try{
-
                     jarr = new JSONArray(jsonObj.get(keyword).toString());
-                } catch (Exception e){ }
+                } catch (Exception e){ };
             }
             else  jarr = new JSONArray(jsonObj.toString());
 
-            max_limit = limit >= jarr.length() ? jarr.length() - 1 : limit;
-
-            for (int i = offset; i <= max_limit; i++) {
-
+            for (int i = offset; i < jarr.length() && limit>=0; i++,limit--) {
                 JSONObject jsonObject = jarr.getJSONObject(i);
-
                 gson = new Gson();
-                nwUs = gson.fromJson(jsonObject.toString(), User.class);
+                try {
+                    nwUs = gson.fromJson(jsonObject.toString(), User.class);
+
+                }catch(Exception e){}
+
+                try{
+                    loc = gson.fromJson(jsonObject.toString(), Location.class);
+                    nwUs.setLocation(loc);
+                }catch(Exception e){}
+
                 userArrayList.add(nwUs);
-                //Log.d("gagagagagag", "foll id:  " + nwUs.getId());
+
             }
+
             return userArrayList;
 
         } catch (JSONException e) {
